6742bb07669defbc9c849b8dcf431c2fe54f3f4c
obre-bjorn <obre.bjorn@gmail.com>
2024-07-28 20:49:40 +0300

    UI done

--- app/src/main/java/com/example/diceroller/MainActivity.kt
+++ app/src/main/java/com/example/diceroller/MainActivity.kt
@@ -4,11 +4,17 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.wrapContentSize
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.*
+import androidx.compose.material3.Button
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.diceroller.ui.theme.DiceRollerTheme
 
 class MainActivity : ComponentActivity() {
@@ -28,15 +34,24 @@ class MainActivity : ComponentActivity() {
 @Preview
 @Composable
 fun DiceRollerApp(){
-    DiceWithButtonAndImage()
+    DiceWithButtonAndImage(modifier = Modifier
+        .fillMaxSize()
+        .wrapContentSize(Alignment.Center))
 }
 
 @Composable
-fun DiceWithButtonAndImage(modifier:Modifier = Modifier
-    .fillMaxSize()
-    .wrapContentSize()
-){
+fun DiceWithButtonAndImage(modifier: Modifier = Modifier) {
 
+    Column (modifier = modifier, horizontalAlignment = Alignment.CenterHorizontally){
+        Image(
+            painter = painterResource(R.drawable.dice_1),
+            contentDescription = "1"
+            )
+        Spacer(modifier = Modifier.height(16.dp))
+        Button(onClick = {/* */}){
+            Text(stringResource(R.string.roll))
+        }
+    }
 }
 
 
